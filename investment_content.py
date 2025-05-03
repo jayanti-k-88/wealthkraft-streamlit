76e8069dec4752351aea4d39b1f0c313fbe43ad3
jayanti-k-88 <jayantik.2024@gmail.com>
2025-05-03 11:46:33 +0530

    Initial commit for Streamlit UI for Finnie app

---
+++ investment_content.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def investment_page():
+    st.subheader("📈 Optimize your Investment Portfolio")
+    st.markdown("""
+    Need some help in optimizing your investment portfolio? Get the most out of the current stock market with an investment portfolio tailored specifically for you. 
+    - Would you like the most optimal investment portfolio with best returns and lowest possible market risk? Get a portfolio with **Maximum Sharpe  Ratio**. 
+    - Or do you have a specific target return in mind? We offer a market neutral portfolio with a rebalancing option for a high **Target Return**.
+    - Or would you like to opt for the least risk portfolio? Stay safe with our **Minimum Volatility** portfolio plan.
+
+    Provide your risk appetite and total investment amount in the chat window.
+    """)
